9b683f384662307f5593f7b5e62282e567d0c8a1
Joao Victor <jov9051@outlook.com>
2022-04-22 20:23:50 -0300

    fix: codigo nao utilizado removido

--- src/index.js
+++ src/index.js
@@ -8,8 +8,6 @@ let sysCore = sysCpus.length;
 
 let totalSysMemory = parseInt(os.totalmem() / 1024 / 1024);
 
-let sysCurrentUser = os.homedir();
-
 let sysDeviceName = os.hostname();
 
 let sysNetworkInterfaces = os.networkInterfaces();
@@ -17,3 +15,16 @@ let sysNetworkInterfaces = os.networkInterfaces();
 let sysOs = os.type();
 
 let sysUserInfo = os.userInfo();
+
+let info = {
+    os: sysOs,
+    architecture: sysArch,
+    cpus: sysCpus,
+    cores: sysCore,
+    memory: totalSysMemory,
+    user: sysUserInfo,
+    device: sysDeviceName,
+    network: sysNetworkInterfaces
+};
+
+console.log(info);
